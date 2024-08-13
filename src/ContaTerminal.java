0f44285e8f4b7eef3782b40812385f7b6599737f
Cristiano-Paz <cristianogspaz@gmail.com>
2024-08-13 18:14:50 -0300

    finishing the exercise

---
+++ src/ContaTerminal.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+
+    public static void main(String[] args) {
+       
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        System.out.println("Enter the account:");
+        int number = scanner.nextInt();
+        
+        
+        scanner.nextLine();
+        
+        System.out.println("Enter the agency:");
+        String agency = scanner.nextLine();
+        
+        System.out.println("Enter the name:");
+        String nameClient = scanner.nextLine();
+        
+        System.out.println("Entering your balance:");
+        double balance = scanner.nextDouble();
+        
+        
+        String message = "Hello " + nameClient + ", Thank you for creating an account at our bank, your branch is " 
+                        + agency + ", account " + number + " your balance " + balance + " is now available to be collected.";
+        System.out.println(message);
+        
+        
+        
+    }
+}
